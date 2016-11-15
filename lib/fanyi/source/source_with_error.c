7235ad06ce18cc0f1dcf3627f90310edc2ef6b78
beim <beim2015@outlook.com>
2016-11-15 11:14:10 +0800

    语义分析v1.3 增加函数定义重复的判断

---
+++ lib/fanyi/source/source_with_error.c
@@ -0,0 +1,20 @@
+int a, b = 1;
+int a;
+float c, d = 1.1;
+
+int a(){}
+int a(){}
+
+/*
+int func1(int a) {
+    float b;
+    int inner(int a) {
+        a = 1;
+    }
+    inner();
+    return b;
+}
+int func1(int a) {
+    return a;
+}
+*/
