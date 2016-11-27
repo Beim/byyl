bc306d4a9a4d97c31ec7ad67c6b439bb0b0d97cc
beim <beim2015@outlook.com>
2016-11-27 15:25:58 +0800

    局部优化初步完成

---
+++ lib/fanyi/source/source.c
@@ -0,0 +1,52 @@
+/*
+int a, b;
+
+a = 1;
+// b = 2;
+
+a = 1 + b;
+b = 2 + a;
+
+if (a < 100 || a > 200 && a != b) {
+    b = 1;
+} else {
+    a = 1;
+}
+
+a = 2;
+a = 3;
+a = 4;
+*/
+
+int a,b,c,d;
+a = b + c;
+a = b + c;
+if (a > 0) {
+    d = a;
+}
+else {
+    d = 10;
+}
+/*
+int a,b,c,d;
+a = b + c;
+if (a > 1) {
+    a = 2;
+}
+else {
+    a = 3;
+}
+*/
+/*
+int a,b;
+int[2] arr;
+a = arr[1];
+arr[1] = 2;
+b = arr[1];
+
+*/
+/*
+int a,b;
+int func(int a, int b) {}
+func(a, b);
+*/
